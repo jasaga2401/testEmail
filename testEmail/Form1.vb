6465f39d5e509d0db337d8e16968ca66e71db1ab
jasaga2401 <jasaga@protonmail.com>
2024-10-17 00:55:13 +0100

    Add project files.

---
+++ testEmail/Form1.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Net
+Imports System.Net.Mail
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim mail As New MailMessage()
+
+        mail.From = New MailAddress("gallcockmail@googlemail.com")
+        mail.To.Add("gallcockmail@googlemail.com")
+        mail.Subject = "Test"
+        Dim htmlBody As String = "<h1 style='color:blue;'>Hello!</h1>" &
+                                 "<p>This is a test email with <b>HTML formatting</b>.</p>" &
+                                 "<p>You can use <a href='https://www.priestley.ac.uk/'>links</a>, " &
+                                 "style text, and format it in many ways!</p>"
+
+        mail.Body = htmlBody
+        mail.IsBodyHtml = True
+
+        Dim smtp As New SmtpClient("smtp.gmail.com")
+        smtp.Port = 587
+        smtp.Credentials = New Net.NetworkCredential("gallcockmail@googlemail.com", "[App Password here]")
+
+        smtp.EnableSsl = True
+        Try
+            smtp.Send(mail)
+            MessageBox.Show("Email sent successfully!")
+        Catch ex As Exception
+            MessageBox.Show("Error: " & ex.Message)
+        End Try
+
+
+
+    End Sub
+
+End Class
